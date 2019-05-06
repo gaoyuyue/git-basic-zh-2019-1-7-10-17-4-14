50e3825a64d3bdfb27ad8ddb9af1ca14d4cfea72
高语越 <gaoyuyue@outlook.com>
2019-05-06 21:20:02 +0800

    add test collect same elements should return an array with same elements in two collections for section1.PracticeC

--- src/main/java/cn/school/thoughtworks/section1/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeC.java
@@ -1,11 +1,13 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PracticeC {
-    List<String> collectSameElements(List<String> collection1, Map<String,List<String>> collection2) {
-        //实现练习要求，并改写该行代码。
-        return null;
+    List<String> collectSameElements(List<String> collection1, Map<String, List<String>> collection2) {
+        return collection1.stream().filter(s -> collection2.getOrDefault("value", new ArrayList<>()).contains(s))
+                .collect(Collectors.toList());
     }
 }
