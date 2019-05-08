2777597b2d4f4a9f5ec1a43ef7b6014524e0bdd2
高语越 <gaoyuyue@outlook.com>
2019-05-08 11:19:43 +0800

    add test count same elements should return an map with elements and count for section2.PracticeB

--- src/main/java/cn/school/thoughtworks/section2/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeB.java
@@ -1,12 +1,30 @@
 package cn.school.thoughtworks.section2;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeB {
     Map<String, Integer> countSameElements(List<String> collection1) {
-        //实现练习要求，并改写该行代码。
+        return collection1.stream().reduce(new HashMap<>(), (acc, s) -> {
+            int count = 1;
+            String[] ss = s.split("-");
+            if (ss.length == 2) {
+                s = ss[0];
+                count = Integer.parseInt(ss[1]);
+            }
+            updateMap(acc, s, count);
+            return acc;
+        }, (acc, item) -> {
+            acc.putAll(item);
+            return acc;
+        });
+    }
 
-        return null;
+    private void updateMap(HashMap<String, Integer> acc, String s, int count) {
+        if (acc.containsKey(s)) {
+            count = acc.get(s) + count;
+        }
+        acc.put(s, count);
     }
 }
