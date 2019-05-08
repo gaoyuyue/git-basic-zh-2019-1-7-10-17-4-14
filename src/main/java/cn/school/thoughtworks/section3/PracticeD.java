03bad13071c6bc4d5df4458da1caebb2cf17f47b
高语越 <gaoyuyue@outlook.com>
2019-05-08 22:28:39 +0800

    add test create update collection should return a new map with element as key and count as value for section3.PracticeD

--- src/main/java/cn/school/thoughtworks/section3/PracticeD.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeD.java
@@ -1,12 +1,41 @@
 package cn.school.thoughtworks.section3;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeD {
     Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
-        //实现练习要求，并改写该行代码。
+        Map<String, Integer> collection3 = collectionA.stream().reduce(new HashMap<>(), (acc, s) -> {
+            int count = 1;
+            String[] ss = s.split("-");
+            if (ss.length == 2) {
+                s = ss[0];
+                count = Integer.parseInt(ss[1]);
+            }
+            updateMap(acc, s, count);
+            return acc;
+        }, (acc, item) -> {
+            acc.putAll(item);
+            return acc;
+        });
 
-        return null;
+        List<String> ss = object.get("value");
+        collection3.keySet().forEach(s -> {
+            if (ss.contains(s)) {
+                Integer count = collection3.get(s);
+                Integer sub = count / 3;
+                collection3.put(s, count - sub);
+            }
+        });
+
+        return collection3;
+    }
+
+    private void updateMap(HashMap<String, Integer> acc, String s, int count) {
+        if (acc.containsKey(s)) {
+            count = acc.get(s) + count;
+        }
+        acc.put(s, count);
     }
 }
